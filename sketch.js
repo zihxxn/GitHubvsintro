11a6a5399e4d267ed54af8507893e1115219ecc9
zihxxn <jihyunban@students.calarts.edu>
2025-04-12 19:41:54 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,6 +1,18 @@
+let stars = [];
+let angle = 0;
+let numStars = 100;
+
+let shootingStar;
+let shootingTimer = 0;
+let shootingActive = false;
+let tail = [];
+
 function setup() {
   // Create a canvas that fills the entire browser window
   createCanvas(windowWidth, windowHeight);
+  createStars(numStars);
+  initShootingStar();
+
 }
 
 function draw() {
@@ -18,4 +30,90 @@ function draw() {
 
   // Draw a circle in the center of the canvas
   ellipse(width / 2, height / 2, 200, 200);
+
+  background(10, 10, 30, 100);
+  drawStars();
+  updateStars();
+  handleShootingStar();
+}
+function createStars(num) {
+  for (let i = 0; i < num; i++) {
+    let star = {
+      x: random(width),
+      y: random(height),
+      size: random(1, 3),
+      baseBrightness: random(150, 255),
+      twinkleSpeed: random(0.02, 0.05),
+      twinkleAngle: random(TWO_PI)
+    };
+    stars.push(star);
+  }
+}
+
+function drawStars() {
+  for (let i = 0; i < stars.length; i++) {
+    let s = stars[i];
+    let alpha = map(sin(s.twinkleAngle), -1, 1, 100, s.baseBrightness);
+    fill(255, 255, 255, alpha);
+    noStroke();
+    circle(s.x, s.y, s.size);
+  }
+}
+
+function updateStars() {
+  for (let i = 0; i < stars.length; i++) {
+    let s = stars[i];
+    s.twinkleAngle += s.twinkleSpeed;
+    if (s.twinkleAngle > TWO_PI) {
+      s.twinkleAngle = 0;
+    }
+  }
+  angle += 0.01;
+}
+
+function initShootingStar() {
+  shootingStar = {
+    x: random(width / 2),
+    y: random(height / 3),
+    speedX: random(6, 10),
+    speedY: random(3, 5),
+    size: random(3, 5),
+    active: false
+  };
+  tail = [];
+}
+
+function handleShootingStar() {
+  if (shootingActive) {
+    tail.push({ x: shootingStar.x, y: shootingStar.y });
+
+    fill(255);
+    noStroke();
+    circle(shootingStar.x, shootingStar.y, shootingStar.size);
+
+    for (let i = 0; i < tail.length; i++) {
+      let alpha = map(i, 0, tail.length, 0, 150);
+      fill(255, 255, 255, 150 - alpha);
+      circle(tail[i].x, tail[i].y, shootingStar.size * 0.8);
+    }
+
+    shootingStar.x += shootingStar.speedX;
+    shootingStar.y += shootingStar.speedY;
+
+    if (tail.length > 15) {
+      tail.shift();
+    }
+
+    if (shootingStar.x > width || shootingStar.y > height) {
+      shootingActive = false;
+      shootingTimer = 0;
+      tail = [];
+    }
+  } else {
+    shootingTimer++;
+    if (shootingTimer > 300 && random(1) < 0.02) {
+      initShootingStar();
+      shootingActive = true;
+    }
+  }
 }
